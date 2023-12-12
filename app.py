df4a56ece1e045797e8e1193bf26100e2eb522bc
Jeremy Kolassa <jeremy.kolassa@aximgeo.com>
2023-12-12 10:15:39 -0500

    Some random script I was practicing with

---
+++ app.py
@@ -0,0 +1,17 @@
+facility = "rebel base"
+location = "Dantooine"
+validity = "is not present"
+print(f"The {facility} {validity} on {location}")
+
+codelangs = []
+codelangs.append("Python")
+codelangs.append("Java")
+codelangs.append("C++")
+codelangs.append("JavaScript")
+codelangs.append("C#")
+codelangs.append("PHP")
+codelangs.append("Ruby")
+codelangs.append("Perl")
+codelangs.append("Go")
+
+print(codelangs)
